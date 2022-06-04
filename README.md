c8d073f070252e122b16e3b6055ee49ff2fed8b2
Minhyuk Oh <maothesolution@gmail.com>
2022-06-04 18:56:16 +0900

    updated from mac

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# crallwing_mao
+# crallwing_mao<br/>
+
+## * GOAL
+### --> 다나와 싸이트에서 제품과 그 가격을 가져와 txt파일로 저장하는 프로그램을 만든다 <br/>
+
+## * ENVIRONMENTS
+### --> OS : mac
+### --> brwoser : chrome
+### --> chrome version : 102<br/>
+### --> python : 3.10
+
+## * NOTICE
+### --> 저장 장소는 사용자 정의로 한다
+### --> 필요한 환경설치를 위하여 beautifulsout4, selenium 우선 설치<br/>
+
--- danawa/main.py
+++ danawa/main.py
@@ -1,8 +1,24 @@
+import os
+os.system('pip3 install --upgrade pip')
+os.system('pip3 install selenium')
+os.system('pip3 install beautifulsoup4')
+
 '''
 다나와 싸이트에서 제품과 그 가격을 가져와 txt파일로 저장하는 프로그램을 만든다
 저장 장소는 사용자 정의로 한다
 필요한 환경설치를 위하여 beautifulsout4, selenium 우선 설치
 '''
-# pip install selenium
-# pip install beautifulsoup4
+
+
+
+import selenium
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
 
