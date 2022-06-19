1a64b10e21bb320c6efd3d5e4d3dbb3abbe84ccc
mao <maothesolution@gmail.com>
2022-06-20 05:27:23 +0900

    + searchCPU   store the titles and their prices in the dictionary, cpu - Needs to make the price as number

--- danawa/main.py
+++ danawa/main.py
@@ -4,22 +4,27 @@ from selenium import webdriver as wd
 from selenium.webdriver.chrome.options import Options
 import chromedriver_autoinstaller
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 
-def readWebpage():
+def readWebpage(url):
     '''
     open the url with Chrome and read the page source without a browser open
     '''
     global option, driver
-    url = 'https://www.danawa.com/'
     chromedriver_autoinstaller.install()        # install chromdriver_autoinstaller
     option = wd.ChromeOptions()                 # open a webpage without a broswer open
     # option.add_argument('headless')
     # driver = wd.Chrome(options=option)
     driver = wd.Chrome()
     driver.get(url)                             # request to open the danawa webpage
-    driver.implicitly_wait(3)                   # wait 3 seconds to load the page
+    driver.implicitly_wait(5)                   # wait 30 seconds to load the page. If it ends early, move on and save time.
+
+    element = driver.find_element(by=By.XPATH, value='//*[@id="danawa_menubar"]/div[2]/a[3]') # open a component page 
+    element.click()
+    parent = driver.window_handles[0]            # set the current driver as parent
+    child = driver.window_handles[1]             # set the new tab a s a child
+    driver.switch_to.window(child)               # move to the child 
 
 def htmlParseUsingSoup():
     '''
@@ -33,9 +38,19 @@ def searchCPU():
     '''
     collect cpu data   
     '''
-    global bfSoup, driver
-    element = driver.find_element_by_id('selectionLayer')
-    element.click()
+    global driver, cpu
+    cpu = dict()
+    for each in range(1, 32):
+        title = driver.find_element(by=By.XPATH, value='//*[@id="estimateMainProduct"]/div/div[2]/div[2]/table/tbody/tr['+ str(each) + ']/td[2]/p/a')
+        price = driver.find_element(by=By.XPATH, value='//*[@id="estimateMainProduct"]/div/div[2]/div[2]/table/tbody/tr['+ str(each) +']/td[3]/p/span')
+        cpu[title.text] = price.text
+
+    for each in cpu.items():
+        print(each)
+
+    # print('PageContents:{}'.format(driver.page_source))
+
+
     # WebDriverWait(driver,3).until(EC.presence_of_element_located((By.XPATH,'//*[@id="categoryUnit97"]/ul/li[1]/a'))).click()
     # html = driver.page_source 
     # bfSoup = BeautifulSoup(html, 'html.parser')
@@ -45,7 +60,7 @@ def searchCPU():
 
 
 def run():
-    readWebpage()
+    readWebpage('https://www.danawa.com/')
     # htmlParseUsingSoup()
     searchCPU()
 
